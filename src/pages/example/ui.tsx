88dcc569628e354bd180ff93e73e7fd21bf2d91d
PashaYakubovsky <yakubovskypasha@gmail.com>
2023-06-12 19:18:35 +0300

    +hide unused props

--- src/pages/example/ui.tsx
+++ src/pages/example/ui.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./ui.module.scss";
 
-const ExampleUI = React.forwardRef<HTMLDivElement>((props, ref) => {
+const ExampleUI = React.forwardRef<HTMLDivElement>((_, ref) => {
     return (
         <div className={style.ui} ref={ref}>
             <div>ExampleUI</div>
